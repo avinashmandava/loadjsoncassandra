328ad0a561faee713b12d2f680d9375b09afbeba
Avinash Mandava <avinash.mandava@gmail.com>
2016-04-18 13:09:53 -0700

    add ip params to cqlsh

--- load.sh
+++ load.sh
@@ -1,3 +1,3 @@
-$1bin/cqlsh -f $3
+$1bin/cqlsh $4 9042 -f $3
 
 python loaddata.py $2 $4 $5
--- loadandindex.sh
+++ loadandindex.sh
@@ -1,4 +1,4 @@
-$1bin/cqlsh -f $3
+$1bin/cqlsh $4 9042 -f $3
 
 sudo $1bin/dsetool create_core $5 solrconfig=./setup/solrconfig.xml schema=./setup/schema.xml
 
